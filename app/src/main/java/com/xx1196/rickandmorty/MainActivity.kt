c5f3cd0905b4a7c1a19c534480b2958742b412b7
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2021-07-31 09:45:23 -0500

    *Privera version de la app

---
+++ app/src/main/java/com/xx1196/rickandmorty/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.xx1196.rickandmorty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}#932
